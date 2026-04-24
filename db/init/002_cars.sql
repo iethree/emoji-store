f906a2d4ff7d912e171444c242b825d9e62cd9e4
Ryan Laurie <iethree@gmail.com>
2026-04-24 13:16:34 -0600

    add cars

---
+++ db/init/002_cars.sql
@@ -0,0 +1,12 @@
+INSERT INTO emojis (symbol, name, price_cents, stock, category) VALUES
+  ('🚗', 'Automobile', 499, 50, 'cars'),
+  ('🚕', 'Taxi', 549, 40, 'cars'),
+  ('🚙', 'Sport Utility Vehicle', 599, 40, 'cars'),
+  ('🏎️', 'Racing Car', 1499, 15, 'cars'),
+  ('🚓', 'Police Car', 649, 30, 'cars'),
+  ('🚑', 'Ambulance', 649, 30, 'cars'),
+  ('🚒', 'Fire Engine', 699, 25, 'cars'),
+  ('🚐', 'Minibus', 579, 35, 'cars'),
+  ('🛻', 'Pickup Truck', 629, 35, 'cars'),
+  ('🚚', 'Delivery Truck', 679, 30, 'cars'),
+  ('🚛', 'Articulated Lorry', 799, 20, 'cars');
